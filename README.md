21187eabc841a269a3f42561b58351adb8eba11d
JeMaGa <jemaga.dev@gmail.com>
2021-04-03 10:46:29 +0200

    improved README and cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # EPUBTOCBZ.PY
+
+
+This is a python3.9 script aiming at converting comics in epub format to CBZ format.
+
+## Why this script
+
+I have recently had a bad customer experience with comics in CBZ format, into which the JPEG files where too compressed for reading (blurry text, artefacts, ...), whereas the EPUB files also provided where readable but not practical. Knowing that EPUB files themselves also contain JPEG files of the comics pages, I made this script in order to be able to read my purchased comics comfortably. If anything, I hope i will not have to use it again by have correct CBZ files directly at purchase.
+## requirements
+
+1. [Python 3.9+](https://www.python.org/)
+2. [Click](https://click.palletsprojects.com/en/5.x/)
+
+## usage
+
+`python3 epubtocbz.py ORIGIN DESTINATION`
+
+where: 
+
++ origin is a directory containing the .epub files to convert
++ destination is a path for the generated files to be put in. If destination does not exist, it will be created.
--- epubtocbz.py
+++ epubtocbz.py
@@ -4,7 +4,12 @@ import shutil
 import zipfile
 from zipfile import ZipFile, is_zipfile
 
-IMG_PATH = 'tmp/OEBPS/images/'
+TMP_PATH = 'tmp/'
+IMG_PATH = 'OEBPS/images/'
+COVER = 'Cover.jpg'
+PAGE_1 = 'page001.jpg'
+EPUB = '.epub'
+CBZ = '.cbz'
 
 
 def epubtocbz(file, origin, destination):
@@ -13,18 +18,18 @@ def epubtocbz(file, origin, destination):
         print(f'{{file}} is not a zip file and cannot be processed.')
     else:
         with ZipFile(origin_path) as origin:
-            origin.extractall(path='tmp/')
-            if os.path.isfile(IMG_PATH + 'Cover.jpg'):
-                os.rename(IMG_PATH + 'Cover.jpg',
-                          IMG_PATH + 'page001.jpg')
+            origin.extractall(path=TMP_PATH)
+            if os.path.isfile(TMP_PATH + IMG_PATH + COVER):
+                os.rename(TMP_PATH+IMG_PATH + COVER,
+                          TMP_PATH+IMG_PATH + PAGE_1)
                 destination_path = destination+'/' + \
-                    file.removesuffix('.epub')+'.cbz'
+                    file.removesuffix(EPUB)+CBZ
                 with ZipFile(destination_path, 'w') as destination_file:
-                    for img_file in os.listdir(IMG_PATH):
+                    for img_file in os.listdir(TMP_PATH+IMG_PATH):
                         destination_file.write(
-                            IMG_PATH+img_file)
+                            TMP_PATH+IMG_PATH+img_file)
             else:
-                print(f'Cover.jpg file not found in file : {{file}}')
+                print(f'{{COVER}} file not found in file : {{file}}')
             shutil.rmtree('tmp')
 
 
@@ -32,13 +37,22 @@ def epubtocbz(file, origin, destination):
 @click.argument('origin')
 @click.argument('destination')
 def main(origin, destination):
-    origin_files = [f for f in os.listdir(origin) if f.endswith('.epub')]
-    if len(origin_files) > 0:
-        # make something
-        for file in origin_files:
-            epubtocbz(file, origin, destination)
+    if os.path.isdir(origin):
+        origin_files = [f for f in os.listdir(origin) if f.endswith('.epub')]
+        if len(origin_files) > 0:
+            # destination exists ?
+            if not os.path.isdir(destination):
+                os.makedirs(destination)
+                print(
+                    f'The destination path {{destination}} has been created.')
+            # make something
+            with click.progressbar(origin_files, label='Processing files', length=len(origin_files)) as bar:
+                for file in bar:
+                    epubtocbz(file, origin, destination)
+        else:
+            print(f'No .epub file found in {{origin}}. Exiting')
     else:
-        print(f'No .epub file found in {{origin}}. Exiting')
+        print(f'{{origin}} is not a directory. Exiting')
 
 
 if __name__ == '__main__':
